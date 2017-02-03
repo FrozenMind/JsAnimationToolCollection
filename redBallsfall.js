3feb505d08a4032c8f73c55433af0f19b009b86b
FrozenMind <valentin.rebscher@live.de>
2017-02-03 19:45:01 +0100

    added a falling ball animation

---
+++ redBallsfall.js
@@ -0,0 +1,48 @@
+var stage;
+var circle;
+var arr = [];
+var ja;
+init();
+
+function init() {
+    stage = new createjs.Stage("demoCanvas");
+    for (i = 0; i <
+        arr.length; i++) {
+        stage.addChild(arr[i]);
+    }
+    ja = 0;
+    createjs.Ticker.addEventListener("tick", tick);
+    createjs.Ticker.setFPS(30);
+}
+var up = false;
+
+function tick() {
+    arr.push(newCircle(Math.floor(Math.random() * (stage.canvas.width - 10) + 1), Math.floor(Math.random() * 6 + 1), Math.floor(Math.random() * 30 + 5)));
+    for (i = arr.length - 1; i >= 0; i--) {
+        arr[i].y += arr[i].speed;
+        if (arr[i].y > stage.canvas.height + 10) {
+            stage.removeChild(arr[i]);
+            arr.splice(i, 1);
+        }
+    }
+    stage.update();
+}
+var r, g, b;
+r = 0;
+g = 0;
+b = 0;
+
+function newCircle(yy, spee, siz) {
+    var cir = new createjs.Shape();
+    r = Math.floor((Math.random() * 255) + 1);
+    g = Math.floor((Math.random() * 255) + 1);
+    b = Math.floor((Math.random() * 255) + 1);
+    g = 0;
+    b = 0;
+    cir.graphics.beginFill("rgb(" + r + "," + g + "," + b + ")").drawCircle(0, 0, siz);
+    cir.x = yy;
+    cir.y = -10;
+    stage.addChild(cir);
+    cir.speed = spee;
+    return cir;
+}
