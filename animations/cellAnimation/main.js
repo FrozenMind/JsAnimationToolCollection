113c52494a42ef6e1774e9852d0f9dc802bbfa75
FrozenMind <valentin.rebscher@live.de>
2017-03-25 13:25:48 +0100

    cellanimation: now new cells are created

--- animations/cellAnimation/main.js
+++ animations/cellAnimation/main.js
@@ -12,7 +12,7 @@ function init() {
     /*
      * SETTING AREA
      */
-    cellAmount = 50;
+    cellAmount = 150;
     cellRadius = 5;
     fps = 60;
     plopCount = 5;
@@ -36,12 +36,14 @@ function init() {
 
 function tick() {
     for (i = cells.length - 1; i >= 0; i--) {
-        //check if cells hit borders
+        //check if cells hit borders, return speed if they do
         if (cells[i].cell.x < 0 + cells[i].radius || cells[i].cell.x > width - cells[i].radius) {
             cells[i].speedX *= -1;
         } else if (cells[i].cell.y < 0 + cells[i].radius || cells[i].cell.y > height - cells[i].radius) {
             cells[i].speedY *= -1;
         }
+        var spliced = false;
+        //check hit with other cells
         for (j = i - 1; j >= 0; j--) {
             var d = Math.sqrt(Math.pow(cells[i].cell.x - cells[j].cell.x, 2) + Math.pow(cells[i].cell.y - cells[j].cell.y, 2));
             if (d < cells[j].radius + cells[i].radius) {
@@ -50,18 +52,19 @@ function tick() {
                 cells[j].cell.graphics.drawCircle(0, 0, cells[j].radius);
                 stage.removeChild(cells[i].cell);
                 cells.splice(i, 1);
+                spliced = true;
                 break;
-
             }
         }
-        cells[i].update();
+        if (!spliced)
+            cells[i].update();
     }
     for (i = cells.length - 1; i >= 0; i--) {
         //cell explodes when its to huge
         if (cells[i].radius > cellRadius * plopCount) {
-            for (j = 0; j < Math.floor(cells[i].radius / cellRadius); j++) {
-                //cells.push(new Cell(cells[i].cell.x, cells[i].cell.y));
-                //TODO: spawn new cells
+            var max = Math.floor(cells[i].radius / cellRadius);
+            for (j = 0; j < max; j++) {
+                cells.push(new Cell(cells[i].cell.x + Math.cos(360 * (j / max - 1)) * cellRadius * 5, cells[i].cell.y + Math.sin(360 * (j / max - 1)) * cellRadius * 5));
             }
             stage.removeChild(cells[i].cell);
             cells.splice(i, 1);
